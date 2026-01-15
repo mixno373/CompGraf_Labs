74e484ca57da5c877fde2e39bdcecb81a3372f7f
Michael <michael@angarsk.info>
2026-01-16 01:00:28 +0800

    Создал библиотеку и класс Shader

---
+++ GrafShaders.cpp
@@ -0,0 +1,13 @@
+#define GLEW_DLL
+#define GLFW_DLL
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+
+#include "GrafShaders.h"
+
+Shader::Shader() {
+
+};
---
+++ GrafShaders.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define GLEW_DLL
+#define GLFW_DLL
+
+#include <iostream>
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+class Shader {
+private:
+
+public:
+};
